15ea248fde24b0ff7c74c1c560cf7ba34ac95a3a
Paul Heintz <pmheintz9@gmail.com>
2018-01-23 21:08:43 -0600

    Complete assignment 2

--- class2/index.php
+++ class2/index.php
@@ -21,6 +21,34 @@
     </p>
 
     <form method="post" action="index.php">
+      <p>Before you start, tell me, did you do Mad Libs as a Kid?<br />
+
+      <?php
+      // Check if radio button has been checked
+      if (isset($_REQUEST['played'])) {
+        $radioBtn = $_REQUEST['played'];
+      } else {$radioBtn = "";}
+      ?>
+
+      <input type="radio" name="played" value="yes" <?php 
+      if ($radioBtn === "yes") {echo 'checked';} ?>> Yes<br />
+      <input type="radio" name="played" value="no" <?php 
+      if ($radioBtn === "no") {echo 'checked';} ?>> No<br />
+      <?php
+      // Echo response to radio button question
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if ($radioBtn === "yes") {
+          echo '<span class="alert">Cool - I did mad libs too</span>';
+        } else if ($radioBtn === 'no') {
+          echo '<span class="alert">Really? You missed out</span>';
+        } else {
+          echo '<span class="alert">Please answer this question?</span>';
+        }
+      }
+      ?>
+      </p>
+
+      <!-- Mad Lib form -->
       <table>
         <tr>
           <td><label>A noun: </label></td><td><input type="text" name="noun1" 
@@ -50,35 +78,42 @@
     </form>
 
     <?php
+      // Process Mad Lib
       if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Check if all fields are populated
-        if(count(array_filter($_REQUEST))!=count($_REQUEST)){
-          echo "<p>* One or more fields were filled with a default word! *</p>";
+        if(empty($_POST['played']) || count(array_filter($_POST))!=count($_POST)){
+          echo '<p class="alert">* Please fill out all fields';
+          if ($radioBtn === 'yes' || $radioBtn === 'no') {
+            echo '. *';
+          } else {
+           echo ' and answer the "Have you played" question. *</p>';
+          }
+        } else {
+
+          // Echo a header
+          echo "<h2>Stairway to Heaven... kind of</h2>".PHP_EOL;
+
+          // Assign values to variables
+          if(!empty($_REQUEST['noun1'])) {$noun1 = $_REQUEST['noun1'];}
+            else {$noun1 = 'lady';}
+          if(!empty($_REQUEST['noun2'])) {$noun2 = $_REQUEST['noun2'];}
+            else {$noun2 = 'gold';}
+          if(!empty($_REQUEST['noun3'])) {$noun3 = $_REQUEST['noun3'];}
+            else {$noun3 = 'stairway';}
+          if(!empty($_REQUEST['location'])) {$location = $_REQUEST['location'];}
+            else {$location = 'heaven';}
+          if(!empty($_REQUEST['places'])) {$places = $_REQUEST['places'];}
+            else {$places = 'stores';}
+
+          // Display new lyrics
+          echo "There's a <b>$noun1</b> who's sure<br />".PHP_EOL;
+          echo "All that glitters is <b>$noun2</b><br />".PHP_EOL;
+          echo "And she's buying a <b>$noun3</b> to <b>$location</b><br />".PHP_EOL;
+          echo "When she gets there she knows<br />".PHP_EOL;
+          echo "If the <b>$places</b> are all closed<br />".PHP_EOL;
+          echo "With a word she can get what she came for<br />".PHP_EOL;
+          echo "Oh oh oh oh and she's buying a <b>$noun3</b> to <b>$location</b><br />".PHP_EOL;
         }
-
-        // Echo a header
-        echo "<h2>Stairway to Heaven... kind of</h2>".PHP_EOL;
-
-        // Assign values to variables
-        if(!empty($_REQUEST['noun1'])) {$noun1 = $_REQUEST['noun1'];}
-          else {$noun1 = 'lady';}
-        if(!empty($_REQUEST['noun2'])) {$noun2 = $_REQUEST['noun2'];}
-          else {$noun2 = 'gold';}
-        if(!empty($_REQUEST['noun3'])) {$noun3 = $_REQUEST['noun3'];}
-          else {$noun3 = 'stairway';}
-        if(!empty($_REQUEST['location'])) {$location = $_REQUEST['location'];}
-          else {$location = 'heaven';}
-        if(!empty($_REQUEST['places'])) {$places = $_REQUEST['places'];}
-          else {$places = 'stores';}
-
-        // Display new lyrics
-        echo "There's a <b>$noun1</b> who's sure<br />".PHP_EOL;
-        echo "All that glitters is <b>$noun2</b><br />".PHP_EOL;
-        echo "And she's buying a <b>$noun3</b> to <b>$location</b><br />".PHP_EOL;
-        echo "When she gets there she knows<br />".PHP_EOL;
-        echo "If the <b>$places</b> are all closed<br />".PHP_EOL;
-        echo "With a word she can get what she came for<br />".PHP_EOL;
-        echo "Oh oh oh oh and she's buying a <b>$noun3</b> to <b>$location</b><br />".PHP_EOL;
       }
     ?>
 
