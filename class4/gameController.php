227f388810929927bd304796ed196123fcd6f7a2
Paul Heintz <pmheintz9@gmail.com>
2018-02-08 21:22:21 -0600

    Add assignment 4

---
+++ class4/gameController.php
@@ -0,0 +1,120 @@
+<?php
+
+// Controller for php hi-low game
+
+// Function to check if guess is within range
+// Returns true if within range, false if not
+function rangeCheck($guess) {
+  $inRange = true;
+  if ($guess < 1 || $guess > 100) {
+    $inRange = false;
+  }
+  return $inRange;
+}  
+
+// Function to check user's guess, increment the count, and add the guess to the prevGuesses array
+// Returns string "high", "low", or "equal" based on user's guess in respect to the target number
+function processGuess($guess) {
+$_SESSION['guessCount']++;
+
+$result = '';
+$prefix = '';
+if ((int)$guess === $_SESSION['targetNumber']) {
+  $result = 'equal';
+  $prefix = '<span class="success">';
+} else if ((int)$guess > $_SESSION['targetNumber']) {
+  $result = 'high';
+  $prefix = '<span class="alert">';
+} else {
+  $result = 'low';
+  $prefix = '<span class="primary">';
+}
+
+array_push($_SESSION['prevGuesses'], $prefix.$guess.'</span>');
+return $result;
+}
+
+// Function to display high scores from file
+function displayHighScores() {
+	if (file_exists('../../uploads/highscores.txt')) {
+		// Read the file data into an array
+		// Strip new lines because they are added with implode() 
+		// Skip empty lines (if written)
+		$_SESSION['highScores'] = file('../../uploads/highscores.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+		foreach ($_SESSION['highScores'] as $val) {
+			$scoreAndName = explode(' ', $val);
+			$name = array_pop($scoreAndName);
+			$score = implode(' ', $scoreAndName);
+			echo '<tr><td>'.$name.'</td><td>'.$score.'</td></tr>'.PHP_EOL;
+			// echo $val.'<br />';
+		}
+	} else {
+		echo 'No high scores yet!';
+	}
+}
+
+// Function to check if high score and update highscores.txt
+function updateHighScores() {
+	$currentGame = $_SESSION['guessCount'].' '.trim($_SESSION['playerName']);
+	if (!isset($_SESSION['highScores'])) {
+		$_SESSION['highScores'] = array();
+	}
+	array_push($_SESSION['highScores'], $currentGame);
+	natsort($_SESSION['highScores']);
+	while (sizeof($_SESSION['highScores']) > 5) {
+		array_pop($_SESSION['highScores']);
+	}
+	writeHighScores();
+}
+
+// Function to write the high scores to a file
+function writeHighScores() {
+	file_put_contents('../../uploads/highscores.txt', implode("\n", $_SESSION['highScores']));
+}
+
+// Check if game is being reset
+if (isset($_POST['reset'])) {
+	session_unset();
+	$_SESSION['guessResult'] = '';
+	$_SESSION['gameOn'] = true;
+}
+
+// Check if session exists, set variables if not
+if (!isset($_SESSION['targetNumber'])) {
+	$_SESSION['targetNumber'] = rand(1, 100);
+	$_SESSION['guessCount'] = 0;
+	$_SESSION['prevGuesses'] = array();
+	$_SESSION['gameOn'] = true;
+	$_SESSION['guessResult'] = '';
+	$_SESSION['playerName'] = '';
+}
+
+// Store player name in session
+if (isset($_POST['playerName'])) {
+	if (empty($_SESSION['playerName'])) {
+		$_SESSION['playerName'] = $_POST['playerName'];
+	} 
+}
+
+// Check if guess has been made
+if (isset($_POST['guess'])){
+    // Check if within range
+    if (rangeCheck($_POST['guess'])) {
+		// Process user's guess
+		$result = processGuess($_POST['guess']);
+		if ($result === 'high') {
+			$_SESSION['guessResult'] = '<p class="alert">'.$_POST['guess'].' is too high</p>';
+		} else if ($result === 'low') {
+			$_SESSION['guessResult'] = '<p class="primary">'.$_POST['guess'].' is too low</p>';
+		} else {
+			$_SESSION['guessResult'] = '<p class="success">Congratulations! '.$_POST['guess'].' is the correct number!<br />'
+                                        .'It took you '.$_SESSION['guessCount'].' guesses.</p>';
+            updateHighScores();
+			$_SESSION['gameOn'] = false;
+		}
+    } else {
+		echo '<script>alert("** OUT OF RANGE **\nPlease enter a whole number between 1-100");</script>';
+    }
+}
+
+?>
---
+++ class4/index.php
@@ -0,0 +1,99 @@
+<?php session_start(); 
+      require 'gameController.php';
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP Hi-Low Game with high scores</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/base.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Paul Heintz</h1>
+      <h4>Class 4 assignment</h4>
+      <h4>2/6/2018</h4>
+      <h4>Hi-Low game with high scores using PHP</h4>
+    </header>
+    <hr />
+
+    <!-- About -->
+    <p>
+      This is the Hi-Low game developed in PHP. A number has been chosen between 1-100. 
+      Keep guessing numbers and you will be told if your number is higher or 
+      lower than the target number . See how many guesses it takes you to find the number! 
+      This new version accesses a file to save and display high scores.
+    </p>
+    <h3>High Scores</h3>
+    <table>
+      <tr>
+        <th>Player Name</th><th>Guesses</th>
+      </tr>
+      <?php displayHighScores(); ?>
+    </table>
+    <hr />
+
+    <!-- Display the number of guesses -->
+    <p>Number of guesses: <b><?php echo $_SESSION['guessCount']; ?></b></p>
+
+    <form action="index.php" method="post">
+      <label>Please enter your name:</label><br />
+      <input type="text" name="playerName" id="player"
+      <?php if (!empty($_SESSION['playerName'])) { 
+        echo 'value="'.$_SESSION['playerName'].'" disabled'; 
+      } else { 
+        echo 'autofocus'; 
+      } ?> /><br /><br />
+      <label>Guess a whole number between 1-100:</label><br />
+      <input type="number" name="guess" id="guessTxt"
+        <?php if($_SESSION['gameOn'] && !empty($_SESSION['playerName'])){echo 'autofocus';} else {echo 'disabled';}?> />
+      <input type="submit" value="Guess" id="guessBtn" <?php if(!$_SESSION['gameOn']){echo 'disabled';}?> />
+    </form>
+    <br />
+
+    <?php
+    // Display guess result (if exists)
+    if (!empty($_SESSION['guessResult'])) {
+      echo $_SESSION['guessResult'];
+    }
+    // Display previous guesses (if exists)
+    if (!empty($_SESSION['prevGuesses'])) {
+      echo '<p>Your previous guesses: ';
+      foreach ($_SESSION['prevGuesses'] as $oldGuess) {
+        echo $oldGuess.' &nbsp; ';
+      }
+      echo '</p>';
+      }
+    ?>
+
+    <!-- Form to reset game (clear session variables) -->
+    <form action="index.php" method="post">
+      <label>Click here to restart: </label>
+      <input type="hidden" name="reset" value="true">
+      <input type="submit" value="Start Over">
+    </form>
+
+    <script type="text/javascript">
+      // Script to ensure that a player name is entered before allowing guesses
+      $(document).ready(function(){
+        // Disable guess button and text field if player name is empty
+        if ($('#player').val().length == 0) {
+          $('#guessTxt').prop('disabled', true);
+          $('#guessBtn').prop('disabled', true);
+        }
+        // On keyup on player name, enable guess fields if player name is not empty
+        $('#player').keyup(function() {
+          if ($('#player').val().length != 0) {
+            $("#guessTxt").prop("disabled", this.value == "" ? true : false);
+            $("#guessBtn").prop("disabled", this.value == "" ? true : false);
+          }
+        });
+      });
+    </script>
+
+  </body>
+</html>
